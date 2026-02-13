ff574df612a47eaad90e361a17cadeda7c24ab0a
parkuskus <ksmaachmad@gmail.com>
2026-02-14 00:26:03 +0700

    refactor: Restructure project files and update application code

---
+++ src/src/main/java/tucil1/aufar/App.java
@@ -0,0 +1,14 @@
+package tucil1.aufar;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        String name = "Aufar" ;
+        int umur = 10 ;
+        System.out.println("Hello World!");
+        System.out.println("Nama saya " + name) ;
+        System.out.println("Umur saya saat ini adalah " + umur) ;
+    }
+}
